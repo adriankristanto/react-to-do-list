ec9dba46e30986e61222bce117ef9671b44b32de
unknown <kristanto.adrian98@gmail.com>
2019-07-23 22:11:34 +1000

    Added uuid package for generating unique id for each todo item

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,74 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from "./TodoList";
 import Footer from "./Footer";
 import InputArea from "./InputArea";
 
-class App extends React.Component {
+// class App extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-      id: 0, // TODO: use uuid package instead
-      data: JSON.parse(localStorage.getItem('todoList')) || []
-    }
-    this.handleItemClick = this.handleItemClick.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleRemoveClick = this.handleRemoveClick.bind(this);
-    this.handleUnload = this.handleUnload.bind(this);
-  }
+//   constructor(){
+//     super();
+//     this.state = {
+//       id: 0, // TODO: use uuid package instead
+//       data: JSON.parse(localStorage.getItem('todoList')) || []
+//     }
+//     this.handleItemClick = this.handleItemClick.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleRemoveClick = this.handleRemoveClick.bind(this);
+//     this.handleUnload = this.handleUnload.bind(this);
+//   }
 
-  handleItemClick(id){
-    this.setState(
-      prev => ({
-        data: prev.data.map(item => id === item.id ? {id: item.id, text: item.text, completed: !item.completed} : item)
-      })
-    )
-  }
+//   handleItemClick(id){
+//     this.setState(
+//       prev => ({
+//         data: prev.data.map(item => id === item.id ? {id: item.id, text: item.text, completed: !item.completed} : item)
+//       })
+//     )
+//   }
 
-  handleSubmit(todoItem){
-    todoItem.replace(/\s/g, '').length && this.setState(
-      prev => ({
-        id: prev.id + 1,
-        data: prev.data.concat({
-          id: prev.id + 1,
-          text: todoItem,
-          completed: false
-        })
-      })
-    );
-  }
+//   handleSubmit(todoItem){
+//     todoItem.replace(/\s/g, '').length && this.setState(
+//       prev => ({
+//         id: prev.id + 1,
+//         data: prev.data.concat({
+//           id: prev.id + 1,
+//           text: todoItem,
+//           completed: false
+//         })
+//       })
+//     );
+//   }
 
-  handleRemoveClick(id){
-    this.setState(
-      prev => ({
-        data: prev.data.filter(item => id !== item.id)
-      })
-    );
-  }
+//   handleRemoveClick(id){
+//     this.setState(
+//       prev => ({
+//         data: prev.data.filter(item => id !== item.id)
+//       })
+//     );
+//   }
 
-  handleUnload(){
-    localStorage.setItem('todoList', JSON.stringify(this.state.data));
-  }
+//   handleUnload(){
+//     localStorage.setItem('todoList', JSON.stringify(this.state.data));
+//   }
 
-  componentDidMount(){
-    window.addEventListener('beforeunload', this.handleUnload)  
-  }
+//   componentDidMount(){
+//     window.addEventListener('beforeunload', this.handleUnload)  
+//   }
 
-  componentWillUnmount(){
-    window.removeEventListener('beforeunload', this.handleUnload);
-  }
+//   componentWillUnmount(){
+//     window.removeEventListener('beforeunload', this.handleUnload);
+//   }
 
-  render(){
+//   render(){
 
-    return (
-      <div className="App">
-        <TodoList handleItemClick={this.handleItemClick} data={this.state.data} handleRemoveClick={this.handleRemoveClick}/>
-        <InputArea handleSubmit={this.handleSubmit}/>
-        <Footer />
-      </div>
-    );
-  }
+//     return (
+//       <div className="App">
+//         <TodoList handleItemClick={this.handleItemClick} data={this.state.data} handleRemoveClick={this.handleRemoveClick}/>
+//         <InputArea handleSubmit={this.handleSubmit}/>
+//         <Footer />
+//       </div>
+//     );
+//   }
 
+// }
+
+function App(){
+  const [id, setId] = useState(0);
 }
 
 export default App;
