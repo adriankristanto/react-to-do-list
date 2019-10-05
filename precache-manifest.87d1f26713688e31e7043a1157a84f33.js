b50fb1fbd0576e1a1f7d898ebf83304906b90c66
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2019-10-05 22:49:33 +1000

    Updates

---
+++ precache-manifest.87d1f26713688e31e7043a1157a84f33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a591fef4dfb1f7006591d960ca4a835a",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "9d132307dbd25c6cdff5",
+    "url": "/react-to-do-list/static/css/main.eb33207d.chunk.css"
+  },
+  {
+    "revision": "69e8efb395374d412c1a",
+    "url": "/react-to-do-list/static/js/2.7515e89d.chunk.js"
+  },
+  {
+    "revision": "9d132307dbd25c6cdff5",
+    "url": "/react-to-do-list/static/js/main.306490fe.chunk.js"
+  },
+  {
+    "revision": "a85afaab04600e454541",
+    "url": "/react-to-do-list/static/js/runtime~main.d9a6f0de.js"
+  }
+]);
