c59c9e7fdad1a34e664d31b30220f071eae9b7ca
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2019-10-05 15:13:23 +1000

    Added show details button

--- to-do-list/src/TodoItem.js
+++ to-do-list/src/TodoItem.js
@@ -4,13 +4,15 @@ import { ThemeContext } from "./ThemeContext";
 import styled from 'styled-components';
 import {Draggable} from 'react-beautiful-dnd';
 // import uuidv4 from 'uuid/v4';
+import SkyLight from 'react-skylight';
 
 const Container = styled.div``
 
 function TodoItem(props){
     const {isLightTheme, lightTheme, darkTheme} = useContext(ThemeContext);
-    const [buttonIsMouseEnter, setButtonIsMouseEnter] = useState(false);
+    const [removeIsMouseEnter, setRemoveIsMouseEnter] = useState(false);
     const [itemIsMouseEnter, setItemIsMouseEnter] = useState(false);
+    const [detailsIsMouseEnter, setDetailsIsMouseEnter] = useState(false);
 
     const todoitemStyle = isLightTheme ? 
     itemIsMouseEnter ? {backgroundColor: lightTheme.itemBackgroundOnHover} : {backgroundColor: lightTheme.itemBackground}
@@ -25,9 +27,13 @@ function TodoItem(props){
         ...todoitemStyle
     }
 
-    const buttonStyle = isLightTheme ? 
-        (buttonIsMouseEnter ? {backgroundColor: lightTheme.itemBackground, color: lightTheme.color} : {backgroundColor: "Transparent", color: lightTheme.color}) : 
-        (buttonIsMouseEnter ? {backgroundColor: darkTheme.itemBackground, color: darkTheme.color} : {backgroundColor: "Transparent", color: darkTheme.color})
+    const removeStyle = isLightTheme ? 
+        (removeIsMouseEnter ? {backgroundColor: lightTheme.itemBackground, color: lightTheme.color} : {backgroundColor: "Transparent", color: lightTheme.color}) : 
+        (removeIsMouseEnter ? {backgroundColor: darkTheme.itemBackground, color: darkTheme.color} : {backgroundColor: "Transparent", color: darkTheme.color})
+    
+    const detailsStyle = isLightTheme ?
+        (detailsIsMouseEnter ? {backgroundColor: lightTheme.itemBackground, color: lightTheme.color} : {backgroundColor: "Transparent", color: lightTheme.color}) : 
+        (detailsIsMouseEnter ? {backgroundColor: darkTheme.itemBackground, color: darkTheme.color} : {backgroundColor: "Transparent", color: darkTheme.color})
 
     const itemStyle = {
         color: !props.todo.completed ? (isLightTheme ? lightTheme.color: darkTheme.color) : "#505050",
@@ -42,7 +48,8 @@ function TodoItem(props){
                     <div className="todo-item" style={snapshot.isDragging ? todoitemStyleDragged : todoitemStyle} onClick={() => props.handleItemClick(props.todo.id)} onMouseEnter={() => setItemIsMouseEnter(true)} onMouseLeave={() => setItemIsMouseEnter(false)} >
                         <input type="checkbox" checked={props.todo.completed} />
                         <p style={itemStyle}>{props.todo.text}</p>
-                        <button onClick={() => props.handleRemoveClick(props.todo.id)} onMouseEnter={()=>setButtonIsMouseEnter(true)} onMouseLeave={()=>setButtonIsMouseEnter(false)} style={buttonStyle}>x</button>
+                        <button id="details" onMouseEnter={() => setDetailsIsMouseEnter(true)} onMouseLeave={() => setDetailsIsMouseEnter(false)} style={detailsStyle}>Show Details</button>
+                        <button id="remove" onClick={() => props.handleRemoveClick(props.todo.id)} onMouseEnter={()=>setRemoveIsMouseEnter(true)} onMouseLeave={()=>setRemoveIsMouseEnter(false)} style={removeStyle}>x</button>
                     </div>
                 </Container>
             )}
